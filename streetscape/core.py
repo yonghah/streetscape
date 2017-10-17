c648092113a79e646b31cd4caa5eee34b6ecd247
Yongha Hwang <yongha.hwang@gmail.com>
2017-10-17 11:50:56 -0400

    filename fix

--- streetscape/core.py
+++ streetscape/core.py
@@ -21,7 +21,7 @@ def get_street_views(lid, lat, lng, save_dir, fov=20, pad=2):
         pitch = "pitch=-5"
         url = "{}&{}&{}&{}&{}".format(base, latlng, view, pitch, key)
         output = os.path.join(save_dir,
-                              "LOC_{}_h{}.jpg".format(lid, heading))
+                              "LOC_{}_h{}.jpg".format(lid, int(heading)))
         if os.path.exists(output):
             os.remove(output)
         res = requests.get(url)
@@ -31,7 +31,8 @@ def get_street_views(lid, lat, lng, save_dir, fov=20, pad=2):
 
 def get_street_views_from_df(df, save_dir="output", geom="obs_point",
                              pic_per_obs=4, pad=2):
-    fov = 360.0 / pic_per_obs;
+    fov = 360.0 / pic_per_obs
+
     def apply_obs_gsv(row):
         lid = row.name
         lat = row[geom].y
@@ -50,14 +51,14 @@ def create_observation_points(city):
         obs_point=lambda x: x.geometry.interpolate(0.5, normalized=True))
     pdf = gpd.GeoDataFrame(line_df['obs_point'],
                            geometry='obs_point',
-                           crs={'init':'epsg:4326'})
+                           crs={'init': 'epsg:4326'})
     return pdf
 
 
 def convert_graph_to_gdf(graph):
     streets = []
     for edge in graph.edges(data=True):
-        street = merge_dicts({'osm_u':edge[0], 'osm_v': edge[1]}, edge[2])
+        street = merge_dicts({'osm_u': edge[0], 'osm_v': edge[1]}, edge[2])
         streets.append(street)
     for street in streets:
         if 'geometry' not in street:
@@ -80,4 +81,3 @@ def merge_dicts(*dict_args):
     for dictionary in dict_args:
         result.update(dictionary)
     return result
-

0682606bd1a0dca035c02ab4007029d252ab6066
Yongha Hwang <yongha.hwang@gmail.com>
2019-01-24 13:12:10 -0500

    add example notebook

--- streetscape/core.py
+++ streetscape/core.py
@@ -9,14 +9,22 @@ import ssl
 import numpy as np
 import pandas as pd
 import aiohttp
+from pypeln import asyncio_task as aio
+from tqdm.autonotebook import tqdm
 
 import geopandas as gpd
 from shapely.geometry import Point
 from shapely.geometry import LineString
 
 
+
 def graph2gdf(graph):
-    '''convert osmnx graph to geodataframe'''
+    '''convert osmnx graph to geodataframe
+    Args:
+        graph (NetworkX.graph): street network from osmnx
+    Returns:
+        converted (GeoDataFrame): street geodataframe
+    '''
     streets = []
     for edge in graph.edges(data=True):
         street = {**{'osm_u': edge[0], 'osm_v': edge[1]}, **edge[2]}
@@ -36,24 +44,18 @@ def graph2gdf(graph):
 def generate_grids(streets, **kwargs):
     '''creates 1d grid along with street segments 
 
-    Arguments:
-        streets: GeoDataFrame
-            geodataframe of street segments
-        distance : float
-            distance between grid 
-        normalized : bool
-            If True, distance is relative value; otherwise absolute.
+    Arg:
+        streets (GeoDataFrame): geodataframe of street segments
+        **distance (float): distance between grid 
+        **normalized (bool): If True, distance is relative value; 
+            otherwise absolute.
             When normalized is True, distance should be [0,1]
-        seg_end : bool
-            if true, force to include both ends of a segment
-        end_gap_dist : float
-            gap threshold between grid point and seg_end
-        segment_id: string
-            name of column contains street segment id
+        **seg_end (bool): if true, force to include both ends of a segment
+        **end_gap_dist (float): gap threshold between grid point and seg_end
+        **segment_id (string): name of column contains street segment id
     
     Returns:
-        grid_gdf: GeoDataFrame 
-            geodataframe for grid points (output)
+        grid_gdf (GeoDataFrame): geodataframe for grid points (output)
 
     Example:
         $ grid_gdf = generate_grids(street_gdf, distance=10)
@@ -69,22 +71,43 @@ def generate_grids(streets, **kwargs):
     return grid_gdf
             
 
-def identify_gsv_locations(grid_gdf):
-    ''' create observataion points which have gsv images'''
-    responses = _retrieve_metadata(grid_gdf)
+def identify_gsv_locations(grid_gdf, max_conn=50, max_sem=10, timeout=0):
+    ''' create observataion points which have gsv images
+    Arguments:
+        grid_gdf (GeoDataFrame): GeoDataFrame for grid points
+        max_conn (int): number of maximum concurrent connections
+        timeout (int): seconds until timeout. 0 unlimted.
+    Returns:
+        res_df (GeoDataFrame): point dataset
+    To do:
+        logging;
+        separate this function into two so that a user can verify
+        the retrieved result;
+        timeout argument;
+    '''
+    responses = _retrieve_metadata(grid_gdf, max_conn, max_sem, timeout)
     res_df = pd.DataFrame.from_records(responses)
     res_df = res_df[res_df['status'] == 'OK']
     res_df['geometry']  = res_df.apply(
         lambda r: Point(r['location']['lng'], r['location']['lat']), 
         axis=1)
     res_df = res_df.drop_duplicates(subset="pano_id").reset_index()
-    res_df = gpd.GeoDataFrame(res_df).drop(columns=['location', 'status'])
+    res_df = gpd.GeoDataFrame(res_df).drop(columns=['location'])
     print(f"{len(res_df)} observation points retrieved.")
     return res_df
 
 
 def make_gsv_urls(gsv_points, **kwargs):
-    ''' create image urls for each heading '''
+    ''' create image urls for each heading 
+    Args:
+        gsv_points (GeoDataFrame): location of gsv shots
+        **npics (int): number of pictures at each location (default 6)
+        **size (int): size of image (image is square)
+        **pad (int): overlapped angle betweeen adjacent image
+        **prefix (str): default prefix for image name (default 'image')
+    Returns:
+        urls (DataFrame): dataframe for google map API url
+    '''
     res = gsv_points.apply(
         lambda x: _make_gsv_urls_row(x, **kwargs), 
         axis=1).sum()
@@ -93,16 +116,23 @@ def make_gsv_urls(gsv_points, **kwargs):
     return urls
 
  
-def download_gsvs(gsv_df, save_dir='', key=''):
-    ''' asynchrounously retrieve gsv images'''
+def download_gsvs(gsv_df, save_dir='', max_conn=50, max_sem=10, timeout=0):
+    ''' asynchrounously retrieve gsv images
+    Args:
+        gsv_df (DataFrame): dataframe for download urls for each image
+        save_dir (str): directory for downloaded images
+        max_conn (int): number of concurrent connections
+        max_sem (int): maximum number of semaphores
+        timeout (int): maximum total running time (0 unlimited)
+    '''
     key = os.environ['GSV_API_KEY']
     
-    async def download_coroutine(session, gsv):
+    async def fetch(session, gsv, sem):
         url = gsv['gsv_url'] + "&key=" + key
         filename = os.path.join(save_dir, gsv['gsv_name'])
 
-        with async_timeout.timeout(10):
-            async with session.get(url) as response:
+        async with session.get(url) as response:
+            async with sem:
                 with open(filename, 'wb') as f_handle:
                     while True:
                         chunk = await response.content.read(1024)
@@ -112,10 +142,23 @@ def download_gsvs(gsv_df, save_dir='', key=''):
                 return await response.release()
 
     async def fetch_all(gsvs, loop):
-        async with aiohttp.ClientSession(loop=loop) as session:
-            for gsv in gsvs:
-                await download_coroutine(session, gsv)
+        conn = aiohttp.TCPConnector(limit=max_conn)
+        timeout_c = aiohttp.ClientTimeout(total=timeout)  
+        sem = asyncio.Semaphore(max_sem)
 
+        async with aiohttp.ClientSession(
+            loop=loop, connector=conn, timeout=timeout_c) as session:
+            
+            tasks = list()
+            for gsv in gsvs:
+                task = asyncio.ensure_future(fetch(session, gsv, sem))
+                tasks.append(task)
+            
+            #results = await asyncio.gather(*tasks, return_exceptions=True)
+            results = [await f
+                for f in tqdm(asyncio.as_completed(tasks), 
+                total=len(tasks))]
+            
     loop = asyncio.get_event_loop()
     loop.run_until_complete(fetch_all(gsv_df.to_records(), loop))
 
@@ -165,26 +208,55 @@ def _image_name(gsv_point, heading, **kwargs):
         "jpg")
 
 
-def _retrieve_metadata(grid_gdf):
+def _retrieve_metadata(grid_gdf, max_conn, max_sem, timeout):
     ''' asynchronous collecting gsv info nearby grid points'''
     urls = list(grid_gdf['metadata_url'])
     key = os.environ['GSV_API_KEY']
 
-    async def fetch(session, url):
+    async def fetch(session, url, sem):
         url = "{}&key={}".format(url, key)
-        async with session.get(url, ssl=ssl.SSLContext()) as response:
-            return await response.json()
+        async with sem:
+            async with session.get(url, ssl=ssl.SSLContext()) as response:
+                return await response.json()
 
     async def fetch_all(urls, loop):
-        async with aiohttp.ClientSession(loop=loop) as session:
-            results = await asyncio.gather(
-                *[fetch(session, url) for url in urls], 
-                return_exceptions=True)
+        conn = aiohttp.TCPConnector(limit=max_conn)
+        timeout_c = aiohttp.ClientTimeout(total=timeout)  # unlimited
+        sem = asyncio.Semaphore(max_sem)
+
+        async with aiohttp.ClientSession(
+            loop=loop, connector=conn, timeout=timeout_c) as session:
+
+            tasks = list()
+            for url in urls:
+                task = asyncio.ensure_future(fetch(session, url, sem))
+                tasks.append(task)
+
+            # results = await asyncio.gather(*tasks, return_exceptions=True)
+            results = [await f
+                for f in tqdm(asyncio.as_completed(tasks), 
+                total=len(tasks))]
+
             return results
 
     loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(fetch_all(urls, loop))
-    return res
+    responses = loop.run_until_complete(fetch_all(urls, loop))
+    records = _filter_errors(responses)
+    
+    return records
+
+
+def _filter_errors(responses):
+    records = []
+    errors = []
+    for res in responses:
+        if type(res) == dict:
+            records.append(res)
+        else:
+            errors.append(res)
+    if len(errors) > 0:
+        print(f'{len(errors)} connection errors')
+    return records
 
 
 def _generate_grids_row(segment, **kwargs):
